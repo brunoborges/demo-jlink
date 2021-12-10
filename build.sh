9205ac1f4cc6b2c87b5eebfc337bb70e473e0148
Bruno Borges <brborges@microsoft.com>
2021-12-10 01:11:38 +0000

    test based on 11 only

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker build -t demoapp:jdk       -f Dockerfile .
-docker build -t demoapp:jre       -f Dockerfile.jre .
-docker build -t demoapp:jre-slim  -f Dockerfile.jre-slim .
-docker build -t demoapp:jlink     -f Dockerfile.jlink .
-docker build -t demoapp:alpine    -f Dockerfile.alpine .
+docker build -t demoapp:jdk           -f Dockerfile.jdk .
+docker build -t demoapp:jdk-alpine    -f Dockerfile.jdk-alpine .
+docker build -t demoapp:jre           -f Dockerfile.jre .
+docker build -t demoapp:jre-slim      -f Dockerfile.jre-slim .
+docker build -t demoapp:jre-alpine    -f Dockerfile.jre-alpine .
+docker build -t demoapp:jlink         -f Dockerfile.jlink .
+docker build -t demoapp:jlink-alpine  -f Dockerfile.jlink-alpine .
