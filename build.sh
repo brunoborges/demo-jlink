ea5df93912a483f844ee350efdbe0dfb186325b1
Bruno Borges <brborges@microsoft.com>
2021-12-10 01:12:44 +0000

    build zulugi

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ docker build -t demoapp:jre-slim      -f Dockerfile.jre-slim .
 docker build -t demoapp:jre-alpine    -f Dockerfile.jre-alpine .
 docker build -t demoapp:jlink         -f Dockerfile.jlink .
 docker build -t demoapp:jlink-alpine  -f Dockerfile.jlink-alpine .
+
+docker build -t demoapp:alpine-zulu-jdk  -f Dockerfile.alpine-zulu-jdk .
+docker build -t demoapp:alpine-zulu-jre  -f Dockerfile.alpine-zulu-jre .
+docker build -t demoapp:alpine-zulu-jlink  -f Dockerfile.alpine-zulu-jlink .
+
